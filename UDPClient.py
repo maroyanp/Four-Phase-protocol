039e91fcfcf6e6868e121b6904f3903c94080feb
Maroyan Putros <31706795+maroyanp@users.noreply.github.com>
2022-02-01 22:07:07 -0500

    Add files via upload

--- UDPClient.py
+++ UDPClient.py
@@ -3,6 +3,55 @@ from socket import *
 import sys # In order to terminate the program
 from struct import *
 
+# returns packetPhaseB1, udp_port, done repeating
+def phaseBclient1SEND(packet, i):
+
+    doneRepeat = False
+    # packet = pack("!IHHIIHH", data_length, PCODE, ENTITY, repeat, udp_port, aLen, codeA)
+
+    data_length, PCODE, ENTITY, repeat, udp_port, aLen, codeA = unpack("!IHHIIHH", packet)
+
+    """
+    struct
+
+    data len 
+    pcode = codeA
+    entity = 1
+    paketid = 0 -> repat -1
+    data -> byte array
+
+    """
+
+    ENTITY = 1
+    data  = bytearray(aLen)
+    PCODEClientB = codeA
+    packetId = i
+
+    if (packetId == repeat - 1):
+        doneRepeat = True
+
+    while (len(data) % 4 != 0):
+        data.append(0)
+
+    data_length = len(data) + 4 + aLen
+
+
+
+    packetBsending = pack("!IHHIs" , data_length, PCODE, ENTITY, packetId,data)
+
+    return packetBsending, udp_port, doneRepeat, packetId
+
+# return i
+def phaseBclient1Rec(packet):
+
+    # packetB1 = pack("!IHHI", data_length, PCODE, ENTITY, ak_packet_id)
+
+    data_length, PCODE, ENTITY, ak_packet_id = unpack("!IHHI")
+    print("Received acknowledgement packet from server: data_len:  {} pcode:  {} entity:  {} acknumber:  {}".format(data_length, ENTITY, ak_packet_id))
+
+    return ak_packet_id
+
+
 
 serverName = 'localhost'
 #serverName = '10.84.88.53'
@@ -20,7 +69,7 @@ PCODE = 0
 struct: 
     
     data_length --> 4 bytes (length of packet)
-    pcode -> first phase == 0
+    pcode -> first phase == 0 (2 bytes)
     entity -> 2 bytes, (client uses 1 and server uses 2)
     data -> any siz % 4 == 0 (data must be padded until divisible by 4)
 
@@ -31,27 +80,49 @@ struct:
 #change socket to be padded to for but for right now this should work ??
 
 x = 0
+data_length = len(sentence)
 # check if divisible by 4
-if (len(sentence) % 4 > 0):
+if (data_length % 4 > 0):
     x = 1
 
-while x:
+while (len(sentence) % 4 !=  0):
     sentence += "0"
-    if (len(sentence) % 4 > 0):
-        x = 1
-    else:
-        x = 0
 
 # after padding we can now send the data
-sentenceStruct = pack('!IIH20s', len(sentence), PCODE, ENTITY, sentence.encode())
+sentenceStruct = pack('!IHH24s', len(sentence), PCODE, ENTITY, sentence.encode())
 
-
+# phase A send
 # -------------------------------------------------------------------------------------------
+# phase B recive
 
 #clientSocket.connect((serverName, serverPort))
 clientSocket.sendto( sentenceStruct, (serverName, serverPort))
-modifiedSentence, serverAddress = clientSocket.recvfrom(2048)
 
-print('From server: ', modifiedSentence.decode())
+packetA, serverAddress = clientSocket.recvfrom(2048)
+
+# creating new socket
+clientSocket2 = socket(AF_INET, SOCK_DGRAM) 
+i = 0
+
+packetBsending, udp_port, doneRepeat, packetId = phaseBclient1SEND(packetA, i)
+
+clientSocket2.sendto( packetBsending, (serverName, udp_port))
+
+while not doneRepeat:
+
+    packetB_rec = clientSocket2.recvfrom(2048)
+
+    akc = phaseBclient1Rec(packetB_rec)
+
+    if(ack == packetId):
+        # send new packetID
+        i += 1
+        packetBsending, udp_port, doneRepeat, packetId = phaseBclient1SEND(packetA, i)
+    else:
+        packetBsending, udp_port, doneRepeat, packetId = phaseBclient1SEND(packetA, i)
+
+
+
+# print('From server: ', modifiedSentence.decode())
 clientSocket.close()
 
--- UDPServer.py
+++ UDPServer.py
@@ -6,18 +6,110 @@ import sys # In order to terminate the program
 from struct import *
 from random import *
 
+# returns udp_port, packetA (struct)
+def phaseA(packet, serverSocket):
+
+	
+
+	data_length, PCODE, ENTITY, data = unpack("!IHH24s", packet)
+
+	checkMsg = "hello world!!!00"
+	temp = data.decode()
+	# temp = temp.replace("0", "")	
+	print(temp + " this is temp")
+	temp2 = temp.strip("0")
+	print(temp2 + " this is t emp2")
+	# checks for server to terminate socket connection
+	if (data_length % 4 != 0 ):
+		print("CLOSING SOCKET")
+		serverSocket.close()
+	if (PCODE != 0):
+		print("CLOSING SOCKET2")
+		serverSocket.close()
+	if (str(temp) == checkMsg.lower()):
+		print(temp)
+		print("CLOSING SOCKET3")
+		print(checkMsg.lower())
+		serverSocket.close()
+	if(ENTITY != 1):
+		print("CLOSING SOCKET4")
+		serverSocket.close()
+
+	repeat = randint(5,20)
+	udp_port = randint(20000, 30000)
+	aLen = randint(50,100)
+	codeA = randint(100, 400)
+	ENTITY = 2
+	"""
+	STRUCT:::::
+
+	data_length ==> len(data)
+	pcode == 0
+	entity ==> 2 
+	repeat ==> random integer (4 bytes) between 5 and 20
+	udp_port ==> random integer (4 bytes) between 20000 and 30000
+	len  ==> short random number between 50 and 100
+	codeA ==> short random number between 100 and 40
+	"""
+
+	print("sending to the client: data_length: {}  code: {}  entity: {} repeat: {} udp_port: {}  len: {} codeA: {}".format(
+		data_length, codeA, ENTITY, repeat, udp_port, aLen, codeA))
+
+	packet = pack("!IHHIIHH", data_length, PCODE, ENTITY, repeat, udp_port, aLen, codeA)
+
+	return udp_port, packet, repeat
+
+	# end of phase a check
+
+#return sendingPacket, repeatDone and PacketB1 (sever reply)
+def phaseBSend(packet, serverSocket, repeat):
+	repeatDone = False
+	sendingPacket = False
+
+	data_length, PCODE, ENTITY, packetID, data = unpack("!IHHIs", packet)
+
+	print("SERVER: received_packet_id =  {} data_len =  {} pcode: {}".format(packetID, data_length, PCODE))
 
 
+	if (data_length % 4 != 0 ):
+		print("CLOSING SOCKET")
+		serverSocket.close()
+	if (PCODE != 0):
+		print("CLOSING SOCKET2")
+		serverSocket.close()
+	if(ENTITY != 1):
+		print("CLOSING SOCKET4")
+		serverSocket.close()
 
-"""
-struct: 
-    
-    data_length --> 4 bytes (length of packet)
-    pcode -> first phase == 0
-    entity -> 2 bytes, (client uses 1 and server uses 2)
-    data -> any siz % 4 == 0 (data must be padded until divisible by 4)
+	ack = randint(0,1)
 
-"""
+
+	if (ack == 1 ): 
+		sendingPacket = True
+	ak_packet_id = packetID
+
+	if (ak_packet_id == repeat -1):
+		repeatDone = True
+
+	"""
+	Struc
+
+	data_len
+	pcode = codeA
+	enty
+	ak_packet_id -> packet id
+
+	"""
+
+	data_length = 4
+	ENTITY = 2
+
+	print("SERVER: received_packet_id =  {} data_len =  {}  pcode: {}".format(ak_packet_id, data_length, PCODE))
+	packetB1 = pack("!IHHI", data_length, PCODE, ENTITY, ak_packet_id)
+
+	return sendingPacket, repeatDone, packetB1;
+	
+
 
 # Assign a port number
 serverPort = 12000
@@ -26,53 +118,41 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 # Bind the socket to server address and server port
 serverSocket.bind(("", serverPort))
 
+# while True:
+
+print('The server is ready to receive')
+
 while True:
 
-	print('The server is ready to receive')
+	firstPass, clientAddress = serverSocket.recvfrom(1024)
+	# we check if it pases phase A
+	udp_port, firstPassPacket, repeat= phaseA(firstPass, serverSocket)
+	serverSocket.sendto(firstPassPacket, clientAddress)
 
-	try:
-		serverSocket.settimeout(3)
+	# listen to new udp_port FIXMEE
+	serverSocket2 = socket(AF_INET, SOCK_DGRAM)
+	serverSocket2.bind(("", udp_port))
 
-		sentenceStruct , clientAddress = serverSocket.recvfrom(1024)
-		data_len, PCODE, ENTITY, data = unpack("!IIH20s", sentenceStruct)
+	secondPass, clientAddress = serverSocket2.recvfrom(1024)
 
-		# check if divisable by 4:
-		if (data_len % 4 != 0):
-			print("message is not within protocol CLOSING CONNECTION")
-			serverSocket.close
-		
 
-		capitalizedSentence = data.decode().upper()
+	sendingPacket, repeatDone, packetB1 = phaseBSend(secondPass, serverSocket2, repeat)
 
-		# phase A::
-		"""
-		repeat is a random integer (4 bytes) between 5 and 20
-		udp_port is a random integer (4 bytes) between 20000 and 30000
-		len is a short random number between 50 and 100
-		codeA is a short random number  between 100 and 400
-		The server then starts listening on the udp_port just sent to the client.
+	while not repeatDone:
 
-		"""
-		repeat = randint(5,20)
-		udp_port = randint(20000, 30000)
-		aLen = randint(50,100)
-		codeA = randint(100, 400)
+		if (sendingPacket):
+			serverSocket2.sendto(secondPass, clientAddress)
+			secondPass, clientAddress = serverSocket2.recvfrom(1024)
+			sendingPacket, repeatDone, packetB1 = phaseBSend(secondPass, serverSocket2, repeat)
+		else:
+			secondPass, clientAddress = serverSocket2.recvfrom(1024)
+			sendingPacket, repeatDone, packetB1 = phaseBSend(secondPass, serverSocket2, repeat)
 
-		phaseA = pack("!IIHH", repeat, udp_port, aLen, codeA )
-		serverSocket.sendto(phaseA, clientAddress)
 
-		# serverSocket.sendto(capitalizedSentence.encode(), clientAddress)
-		
-	except:
-		print("timed out connection nothing recived")
-		serverSocket.close()
-		break
-		
-	
 
-	
-	
-	
+
+
+
 
 serverSocket.close()  
 sys.exit() 	#Terminate the program after sending the corresponding data
